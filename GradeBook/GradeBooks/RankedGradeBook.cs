ac91d7132dd33e0502ddab2875e618688bdde9d3
apautvein <apautvein@gmail.com>
2020-09-26 11:38:51 -0500

    fixed calculation issues

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -20,19 +20,21 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+                throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
             }
 
-            if (averageGrade >= 90)
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold - 1])
                 return 'A';
-            else if (averageGrade >= 80)
+            if (averageGrade >= grades[(threshold * 2) - 1])
                 return 'B';
-            else if (averageGrade >= 70)
+            if (averageGrade >= grades[(threshold * 3) - 1])
                 return 'C';
-            else if (averageGrade >= 60)
+            if (averageGrade >= grades[(threshold * 4) - 1])
                 return 'D';
-            else
-                return 'F';
+            return 'F';
 
 
         }
